9216d220676243610b57b4dba5e8223805f7f2cd
ponkeeto <ponchsalvador@gmail.com>
2023-04-19 16:57:56 +0800

    add Playlist component

---
+++ jammming/src/Components/Playlist/Playlist..js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Playlist.css";
+import TrackList from "../TrackList/TrackList.js";
+
+function Playlist(props) {
+  const handleNameChange = (event) => {
+    props.onNameChange(event.target.value);
+  };
+
+  return (
+    <div className="Playlist">
+      <input defaultValue="New Playlist" onChange={handleNameChange} />
+      <TrackList
+        tracks={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
+      <button className="Playlist-save" onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+}
+
+export default Playlist;
