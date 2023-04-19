1d96186cbc4f4e195394ba9749213f47065c5deb
ponkeeto <ponchsalvador@gmail.com>
2023-04-19 17:07:00 +0800

    add TrackList component

---
+++ jammming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TrackList.css";
+import Track from "../Track/Track";
+
+function TrackList(props) {
+  return (
+    <div className="TrackList">
+      {props.tracks &&
+        props.tracks.map((track) => {
+          return (
+            <Track
+              track={track}
+              key={track.id}
+              onAdd={props.onAdd}
+              onRemove={props.onRemove}
+              isRemoval={props.isRemoval}
+            />
+          );
+        })}
+    </div>
+  );
+}
+
+export default TrackList;
